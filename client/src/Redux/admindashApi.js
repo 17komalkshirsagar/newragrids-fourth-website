28b7fb1e482d13fc88d57e16bcaf49da6ce9cd5f
koam@maticui <koam@maticui.com>
2025-12-10 18:55:55 +0530

    baseurl fix

--- client/src/Redux/admindashApi.js
+++ client/src/Redux/admindashApi.js
@@ -2,37 +2,37 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export const AdminDashApi = createApi({
     reducerPath: "Admindashapi",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/substations",  credentials: "include", }),
+    baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_BACKEND_URL}/substations`, credentials: "include", }),
     tagTypes: ["Admindashapi"],
     endpoints: (builder) => {
         return {
-             getAllPartners: builder.query({
-            query: () => ({
-                url: "/partners",
-                method: "GET",
-                credentials: "include",
+            getAllPartners: builder.query({
+                query: () => ({
+                    url: "/partners",
+                    method: "GET",
+                    credentials: "include",
+                }),
+                providesTags: ["Admindashapi"],
             }),
-            providesTags: ["Admindashapi"],
-        }),
 
-         getAllAssociates: builder.query({
-            query: () => ({
-                url: "/associates",
-                method: "GET",
-                credentials: "include",
+            getAllAssociates: builder.query({
+                query: () => ({
+                    url: "/associates",
+                    method: "GET",
+                    credentials: "include",
+                }),
+                providesTags: ["Admindashapi"],
             }),
-            providesTags: ["Admindashapi"],
-        }),
 
-        getAllEpc: builder.query({
-            query: () => ({
-                url: "/epc",
-                method: "GET",
-                credentials: "include",
+            getAllEpc: builder.query({
+                query: () => ({
+                    url: "/epc",
+                    method: "GET",
+                    credentials: "include",
+                }),
+                providesTags: ["Admindashapi"],
             }),
-            providesTags: ["Admindashapi"],
-        }),
-        
+
         }
     }
 })
